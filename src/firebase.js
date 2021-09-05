103f6347fefa0a239b07bfe9549be1f8db834f58
AlexanderBaikal <abaikaler@mail.ru>
2021-09-05 20:45:47 +0800

    key required

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import firebase from "firebase";
 import "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCP9DAQpH2H3aAqffJzhImNdqM0G2k0HaI",
+  apiKey: "AIzaSyD8JYEGPWAk3URkNgJqiYYbjI-4vxxb_rg",
   authDomain: "g-maps-clone.firebaseapp.com",
   projectId: "g-maps-clone",
   storageBucket: "g-maps-clone.appspot.com",
   messagingSenderId: "38255311699",
-  appId: "1:38255311699:web:73821598771c934fe93fa6",
+  appId: "1:38255311699:web:73821598771c934fe93fa6"
 };
 
 firebase.initializeApp(firebaseConfig);
---
+++ src/firestore.js
@@ -0,0 +1,15 @@
+const firebase = require("firebase")
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCP9DAQpH2H3aAqffJzhImNdqM0G2k0HaI",
+    authDomain: "g-maps-clone.firebaseapp.com",
+    projectId: "g-maps-clone",
+    storageBucket: "g-maps-clone.appspot.com",
+    messagingSenderId: "38255311699",
+    appId: "1:38255311699:web:73821598771c934fe93fa6",
+  };
+  
+  firebase.initializeApp(firebaseConfig);
+
+
+console.log('jjj');
